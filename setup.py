a785cc6f7123bd9910cc113df05e1bdcb4c35609
Narek Maloyan <narek1110@gmail.com>
2021-05-23 13:05:23 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for easier training and preparing medical tools based on deep learning',
+    author='Narek Maloyan',
+    license='MIT',
+)
