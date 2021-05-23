843496b41deef5e105ba01abdab80865e88a2dd6
Narek Maloyan <narek1110@gmail.com>
2021-05-23 14:56:14 +0300

    dataset

--- medtool/data/feta_dataset.py
+++ medtool/data/feta_dataset.py
@@ -1,13 +1,15 @@
 import os
 
+import nibabel as nib
+import torch
 from torch.utils.data import Dataset
 
 def get_path(dir):
     path_dict = {
-        'dseg.json': [],
-        'dseg.nii': [],
-        'T2w.json': [],
-        'T2w.nii': []
+        "dseg.json": [],
+        "dseg.nii": [],
+        "T2w.json": [],
+        "T2w.nii": []
     }
     for i in range(1, 81):
         patient_path = os.path.join(
@@ -16,11 +18,13 @@ def get_path(dir):
             "anat"
         )
         patient_files = os.listdir(patient_path)
-        for p in patient_files:
-            for k in path_dict.keys():
-                if k in p:
-                    path_dict[k].append(p)
-    return path_dict
+        for patient_file in patient_files:
+            for key in path_dict.keys():
+                if key in patient_file:
+                    path_dict[key].append(
+                        os.path.join(patient_path, patient_file)
+                    )
+    return path_dict["T2w.nii"], path_dict["dseg.nii"]
 
 class FeTA(Dataset):
     def __init__(
@@ -36,15 +40,17 @@ class FeTA(Dataset):
         return len(self.data)
 
     def __getitem__(self, item):
-        image  = self.data[item]
-        
+        image_path  = self.data[item]
+        image = nib.load(image_path).get_fdata()
+
         if self.labels:
-            labels = self.labels[item]
+            labels_path = self.labels[item]
+            labels = nib.load(labels_path).get_fdata()
             return {
-                'image': image,
-                'labels': labels 
+                'image':  torch.tensor(image, dtype=torch.float),
+                'labels': torch.tensor(labels, dtype=torch.float)
             }
         else:
             return {
-                'image': image
+                'image':  torch.tensor(image, dtype=torch.float)
             }
--- medtool/train.py
+++ medtool/train.py
@@ -6,4 +6,9 @@ import data.feta_dataset as feta_dataset
 with open(sys.argv[1], 'r') as f:
     config = json.load(f)
 
-print(feta_dataset.get_path(config['data_path']))
+data, labels = feta_dataset.get_path(config['data_path'])
+dataset = feta_dataset.FeTA(
+    data, 
+    labels
+)
+import IPython; IPython.embed(); exit(1)
